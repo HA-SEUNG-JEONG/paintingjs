f6174f58cd6d6933fe629953018c9579319573ea
HA-SEUNG-JEONG <gktmd652@naver.com>
2021-09-02 10:59:23 +0900

    Painting line

---
+++ app.js
@@ -0,0 +1,43 @@
+const canvas=document.getElementById("jsCanvas");
+const ctx=canvas.getContext("2d");
+
+canvas.width=700;
+canvas.height=700;
+
+ctx.strokeStyle="#2c2c2c";
+ctx.lineWidth=2.5;
+
+let painting=false;
+
+
+function StopPainting(){
+    painting=false;
+}
+
+function startPainting(){
+    painting=true;
+}
+
+function onMouseMove(event){
+    const x=event.offsetX;
+    const y=event.offsetY;
+    if(!painting){
+        ctx.beginPath();    //클릭하지 않고 마우스를 움직였을 때는 path 시작
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();   //마우스를 움직이는 내내 발생
+    }
+}
+
+function onMouseDown(event){
+    painting=true;
+}
+
+
+if(canvas){
+    canvas.addEventListener("mousemove",onMouseMove);
+    canvas.addEventListener("mousedown",startPainting);
+    canvas.addEventListener("mouseup",StopPainting);
+    canvas.addEventListener("mouseleave",StopPainting);
+}
