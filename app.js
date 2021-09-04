2a2b52961db7babe096c5937fe1ccfa9b9223aec
HA-SEUNG-JEONG <gktmd652@naver.com>
2021-09-04 12:23:39 +0900

    save 버튼 클릭시 저장하도록 설정

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx=canvas.getContext("2d");
 const colors=document.getElementsByClassName("jsColor");
 const range=document.getElementById("jsRange")
 const mode=document.getElementById("jsMode");
+const saveBtn=document.getElementById("jsSave")
 
 const INITIAL_COLOR="#2c2c2c"
 const CANVAS_SIZE=700;
@@ -69,12 +70,27 @@ function handleCanvasClick(){
     }
 }
 
+
+function handleCM(event){
+    event.preventDefault()
+}
+
+
+function handleSaveClick(){
+    const image=canvas.toDataURL();
+    const link=document.createElement("a");
+    link.href=image;
+    link.download="PaintJS"
+    link.click();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",StopPainting);
     canvas.addEventListener("mouseleave",StopPainting);
-    canvas.addEventListener("click",handleCanvasClick)
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
 Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClick))
@@ -86,4 +102,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click",handleModeClick)
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick)
 }
